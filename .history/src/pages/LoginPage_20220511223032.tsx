0308c200083ed343a41f363bffe5cbfcadc5acdb
Victor Donizete <victordonizete65@gmail.com>
2022-11-08 12:02:20 -0300

    100% commit done (account migration)

---
+++ .history/src/pages/LoginPage_20220511223032.tsx
@@ -0,0 +1,52 @@
+import '../styles/login-page.scss'
+
+import googleIconIMG from '../assets/login-page/googleicon.svg' 
+
+export function LoginPage() {
+  return (
+    <div id="auth">
+      <main>
+        <div className="main-content grid">
+          <div className="welcome">
+            <strong>WELCOME BACK</strong>
+            <p>Welcome back! Please enter your detalis</p>
+          </div>
+         
+          <div className="form">
+            <p>Email</p>
+            <input type='text' name='Email' placeholder='Enter your email' />
+             
+
+            
+            <p>Password</p>
+            <input type="text" name='Password' placeholder='*******'
+            checked />
+
+          </div>
+          <div className="checkbox">
+            <input type="checkbox"/>
+            <label className='label' htmlFor="Password">Remember me</label>
+
+            <a href="#">Forgot password</a>
+          </div>
+          <button className='SignIn'>
+            Sign In
+          </button>
+          <div className="SignIn-google">
+            <button>
+              <i><img src={googleIconIMG} alt="" /></i>
+              Sign in with Google
+            </button>
+          </div>
+
+          <p>Don't have an account? <a href="#">Sign up for free!</a></p>
+
+        </div>
+      </main>
+      <aside>
+        
+
+      </aside>
+    </div>
+  )
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Login-Page
+
+Pagina de login criada em React, css, javaScript
+
+<img src="./public/README/page-home.png" alt="">
